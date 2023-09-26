3c6ee85f410f09822c1a75e9a568a7cd08cb6c45
catseyesolutions <smyrna75@gmail.com>
2023-09-26 10:14:11 +0300

    Update scrapeLogic.js

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -1,19 +1,97 @@
-const puppeteer = require("puppeteer");
+const puppeteer = require("puppeteer-extra");
+const pluginStealth = require("puppeteer-extra-plugin-stealth"); 
 require("dotenv").config();
+const { createWorker } = require('tesseract.js');
+const randomUseragent = require('random-useragent');
+
+puppeteer.use(pluginStealth());
+
+
+const USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36';
+ 
 
 const scrapeLogic = async (res, req) => {
 
-  const browser = await puppeteer.connect({
-    browserWSEndpoint: `wss://chrome.browserless.io?token=7d4132c8-06b0-4b92-b809-388609642558`,
+  const worker = await createWorker({
+    logger: m => console.log(m)
   });
+
+  const browser = await puppeteer.launch({headless: true});
   
   try {
-    const page = await browser.newPage();
-    
-    await page.goto(req.query.url, {
-      waitUntil: 'networkidle0',
-    });
-    
+
+   //Randomize User agent or Set a valid one
+   const userAgent = randomUseragent.getRandom();
+   const UA = userAgent || USER_AGENT;
+   const page = await browser.newPage();
+
+   //Randomize viewport size
+   await page.setViewport({
+       width: 1920 + Math.floor(Math.random() * 100),
+       height: 3000 + Math.floor(Math.random() * 100),
+       deviceScaleFactor: 1,
+       hasTouch: false,
+       isLandscape: false,
+       isMobile: false,
+   });
+
+   await page.setUserAgent(UA);
+   await page.setJavaScriptEnabled(true);
+   await page.setDefaultNavigationTimeout(0);
+
+   //Skip images/styles/fonts loading for performance
+   await page.setRequestInterception(true);
+   page.on('request', (req) => {
+       if(req.resourceType() == 'stylesheet' || req.resourceType() == 'font' || req.resourceType() == 'image'){
+           req.abort();
+       } else {
+           req.continue();
+       }
+   });
+
+   await page.evaluateOnNewDocument(() => {
+       // Pass webdriver check
+       Object.defineProperty(navigator, 'webdriver', {
+           get: () => false,
+       });
+   });
+
+   await page.evaluateOnNewDocument(() => {
+       // Pass chrome check
+       window.chrome = {
+           runtime: {},
+           // etc.
+       };
+   });
+
+   await page.evaluateOnNewDocument(() => {
+       //Pass notifications check
+       const originalQuery = window.navigator.permissions.query;
+       return window.navigator.permissions.query = (parameters) => (
+           parameters.name === 'notifications' ?
+               Promise.resolve({ state: Notification.permission }) :
+               originalQuery(parameters)
+       );
+   });
+
+   await page.evaluateOnNewDocument(() => {
+       // Overwrite the `plugins` property to use a custom getter.
+       Object.defineProperty(navigator, 'plugins', {
+           // This just needs to have `length > 0` for the current test,
+           // but we could mock the plugins too if necessary.
+           get: () => [1, 2, 3, 4, 5],
+       });
+   });
+
+   await page.evaluateOnNewDocument(() => {
+       // Overwrite the `languages` property to use a custom getter.
+       Object.defineProperty(navigator, 'languages', {
+           get: () => ['en-US', 'en'],
+       });
+   });
+
+    await page.goto(req.query.url);
+
     const html = await page.content();
   
     res.send({
