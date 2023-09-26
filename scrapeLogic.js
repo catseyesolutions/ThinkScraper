698b869b0feb66334d138b4b8173f13ce9d8c203
catseyesolutions <smyrna75@gmail.com>
2023-09-26 10:31:08 +0300

    Update scrapeLogic.js

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -7,13 +7,8 @@ puppeteer.use(pluginStealth());
 
 
 const USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36';
- 
 
 const scrapeLogic = async (res, req) => {
-
-  const worker = await createWorker({
-    logger: m => console.log(m)
-  });
 
   const browser = await puppeteer.launch({headless: true});
   
