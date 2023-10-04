d5e67e896373c7a5c6bd6f2a394c49df92c12ec5
catseyesolutions <smyrna75@gmail.com>
2023-10-04 11:37:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const http = require("http");
+const WebSocket = require("ws");
+
 const { scrapeLogic } = require("./scrapeLogic");
 const app = express();
+
+const server = http.createServer(app);
 
 const PORT = process.env.PORT || 4000;
 
@@ -11,6 +16,26 @@ app.get("/scrape", (req, res) => {
 app.get("/", (req, res) => {
   res.send("Render Puppeteer server is up and running!");
 });
+
+//initialize the WebSocket server instance
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws) => {
+
+   console.log('established');
+
+    //connection is up
+    ws.on('message', (message) => {
+
+        //log the received message and send it back to the client
+        console.log('received: %s', message);
+        ws.send(`Hello, you sent -> ${message}`);
+    });
+
+    //send immediatly a feedback to the incoming connection    
+    ws.send('Hi there, I am a WebSocket server');
+});
+
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
