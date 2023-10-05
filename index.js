a81497a6d3a829af1bd9f5e86e7e5a04870a71fd
catseyesolutions <smyrna75@gmail.com>
2023-10-05 09:52:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,16 @@
 const express = require("express");
-const http = require("http");
-const cors = require("cors");
 const { scrapeLogic } = require("./scrapeLogic");
 const app = express();
-const server = http.createServer(app);
-const io = require('socket.io')(server);
-const PORT = process.env.PORT || 5000;
 
-app.use(cors);
+const PORT = process.env.PORT || 4000;
 
 app.get("/scrape", (req, res) => {
   scrapeLogic(res, req);
 });
 
-io.on('connection', (socket) => {
-  console.log('user connected');
-  socket.on('disconnect', function () {
-    console.log('user disconnected');
-  });
-  io.emit('update','test');
-})
+app.get("/", (req, res) => {
+  res.send("Render Puppeteer server is up and running!");
+});
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
