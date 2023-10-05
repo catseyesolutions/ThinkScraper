4bb620a1947b842bb90d072f0994a1b9831e8b5b
catseyesolutions <smyrna75@gmail.com>
2023-10-05 12:32:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const http = require("http");
 const WebSocket = require("ws");
-var Pusher = require("pusher");
 const { scrapeLogic } = require("./scrapeLogic");
 const app = express();
 
