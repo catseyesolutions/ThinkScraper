e44db791e59aa8baa9eb02026d53578424ac6462
catseyesolutions <smyrna75@gmail.com>
2023-11-04 11:17:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const { scrapeLogic } = require("./scrapeLogic");
 const app = express();
 
 const server = http.createServer(app);
+server.timeout = 1000;
 
 const PORT = process.env.PORT || 3000;
 
